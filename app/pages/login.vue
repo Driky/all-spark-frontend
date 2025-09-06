dd3553ec5ea6ae68932765176b8c8e96dbfc4823
Cedric Chatelain <cedric.chatelain@protonmail.com>
2025-09-06 16:02:44 -0400

    feat(login): Adds login form
    Adds a login form component with email and password fields, utilizing UForm and UInput components.
    Includes validation using Zod and emits a "login" event upon submission.
    Also adds a login page that consumes the login form component.

---
+++ app/pages/login.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const handleLogin = async (form: { email: string; password: string }) => {
+    console.log(form);
+};
+</script>
+
+<template>
+  <LoginForm @login="handleLogin" />
+</template>
