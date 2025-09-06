f49e448f03ba0542286ef8c6f7dbd59a49727c26
Cedric Chatelain <cedric.chatelain@protonmail.com>
2025-09-06 16:03:19 -0400

    feat(registration): Adds registration form
    Adds a new registration form component with client-side validation using Zod.
    The form includes fields for name, email, password, and confirm password.
    It also includes basic UI elements from Nuxt UI, and emits a 'register'
    event when the form is submitted. A new register page is also created to
    house the new RegisterForm component.

---
+++ app/pages/register.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const handleRegister = async (form: {
+  name: string;
+  email: string;
+  password: string;
+}) => {
+  console.log(form);
+};
+</script>
+
+<template>
+  <RegisterForm @register="handleRegister" />
+</template>
